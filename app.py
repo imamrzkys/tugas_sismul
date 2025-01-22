ff0faae5ea28e31561d69b111ee813f58fa644aa
imamrzkys <159010421+imamrzkys@users.noreply.github.com>
2025-01-22 16:03:15 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+import os
+import logging
 
 app = Flask(__name__)
 
@@ -31,4 +33,5 @@ def my_account():
     return render_template("my-account.html")
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)

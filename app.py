c28abe1db4d00cd1ec176d18cd5f09aadf50617e
imamrzkys <159010421+imamrzkys@users.noreply.github.com>
2025-01-22 15:23:04 +0700

    MochiLilyShop
    web design

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact-us.html")
+
+@app.route("/shop")
+def shop():
+    return render_template("shop.html")
+
+@app.route("/cart")
+def cart():
+    return render_template("cart.html")
+
+@app.route("/checkout")
+def checkout():
+    return render_template("checkout.html")
+
+@app.route("/my-account")
+def my_account():
+    return render_template("my-account.html")
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5000)
